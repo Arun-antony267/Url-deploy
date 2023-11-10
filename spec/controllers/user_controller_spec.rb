8e1e451763bed7002f804d2570cffd98f6104c56
Arun <arun.antony@geckolyst.com>
2023-11-10 13:17:01 +0530

    commit

--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -1,12 +1,13 @@
 require "rails_helper"
 
   RSpec.describe UserController, type: :controller do
+    user_params = {email: "kiran@gmail.com", password: "123", name: "kiran"}
 
     describe "GET create" do
         it "from a create user" do
-          user_params = {email: "kiran@gmail.com", password: "123", name: "kiran"}
           @user = User.create(user_params)
            user_check = @user.save
+           
           # post :create, params: user_params   
           # user = build(:user)
           # post :create, FactoryBot.create(:user)        
@@ -19,14 +20,16 @@ require "rails_helper"
 
   describe "GET Login" do
     it "from a login user" do
-      # user_params = {email: 'iop@io.com', password: '123' }
+      user_params = {email: 'iop@io.com', password: '123' }
+      @user = User.create(user_params)
+      user_check = User.find_by(email: 'iop@io.com')
+      user = user_check.present?
 
-      # user_check = User.find_by(email: 'iop@io.com')
-      post :check_login, params:{email: 'iop@io.com', password: '123' }
+      # post :check_login, params:{email: 'iop@io.com', password: '123' }
       # user = FactoryBot.create(user: user_params)
       # post :check_login, (user)
 
-      expect(flash[:message]).to eq("Logged In Succesfully")
+      expect( user).to eq(true)
     end
   end
  end
