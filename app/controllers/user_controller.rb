046bf548ec28e19a76b567839fcd97b31ced89b9
Arun <arun.antony@geckolyst.com>
2023-11-10 14:48:46 +0530

    commit

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,6 +1,5 @@
 class UserController < ApplicationController
-  skip_before_action :verify_authenticity_token
-  
+
   def signup 
 
       @user = User.new
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,6 @@ class User < ApplicationRecord
 
   has_secure_password 
 
-  validates :name, uniqueness: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true,
                uniqueness: { case_sensitive: false },
