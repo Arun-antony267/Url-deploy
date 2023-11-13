5a8f847143988ccd015cb2db784b6c088cbc0717
Arun <arun.antony@geckolyst.com>
2023-11-13 11:11:44 +0530

    commit

--- app/controllers/concerns/current_user_concern.rb
+++ app/controllers/concerns/current_user_concern.rb
@@ -11,9 +11,9 @@ module CurrentUserConcern
       end
     end
 
-    def CurrentUserConcern
-      if session[:user_id]
-        @current_user = User.find(session[:user_id])
-      end
-    end
+    # def CurrentUserConcern
+    #   if session[:user_id]
+    #     @current_user = User.find(session[:user_id])
+    #   end
+    # end
   end
