e7b64bef41921a7f2b8ce50fa5962c068ed1e6da
Arun <arun.antony@geckolyst.com>
2023-11-14 12:47:53 +0530

    commit

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,6 @@
 # Load the Rails application.
 require_relative "application"
-require File.expand_path('../config/environment', __dir__)
+# require File.expand_path('../config/environment', __dir__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
