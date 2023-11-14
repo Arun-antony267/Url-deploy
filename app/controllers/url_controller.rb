51fb9fb412338978c0aff05e1439cd1ba68b6be5
Arun <arun.antony@geckolyst.com>
2023-11-14 14:52:34 +0530

    commit

--- app/controllers/url_controller.rb
+++ app/controllers/url_controller.rb
@@ -1,6 +1,6 @@
 class UrlController < ApplicationController
 
-  skip_before_action :verify_authenticity_token
+  # skip_before_action :verify_authenticity_token
 
 def new
   original_url = params[:input]
