afe575a15c7b4fe52e83d55211f6dff789d6d68e
Arun <arun.antony@geckolyst.com>
2023-11-15 11:31:11 +0530

    commit

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -65,10 +65,10 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-#   config.include Capybara::DSL
-# config.include Capybara::RSpecMatchers
+  config.include Capybara::DSL
+config.include Capybara::RSpecMatchers
 
-  config.include Devise::Test::ControllerHelpers, :type => :controller 
-  config.include Warden::Test::Helpers 
+  # config.include Devise::Test::ControllerHelpers, :type => :controller 
+  # config.include Warden::Test::Helpers 
 
 end
