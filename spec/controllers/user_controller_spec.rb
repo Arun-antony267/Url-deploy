09b30c78c5c0d73baae02603ab0dfea4adb7b31c
Arun <arun.antony@geckolyst.com>
2023-11-15 11:19:19 +0530

    commit

--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -1,35 +1,55 @@
 require "rails_helper"
 
+
 RSpec.describe "UserLoginAndSignup" do
-    user_params = {email: "kiran@gmail.com", password: "123", name: "kiran"}
+  user_params = {email: "kiran@gmail.com", password: "123", name: "kiran"}
+  user = User.create(user_params)
+
+  describe "GET create" do
+      it "from create user" do
+         @user = User.find_by(email:"kiran@gmail.com") 
+         user_check = @user.save
+         
+        # post :create, params: user_params   
+        # user = build(:user)
+        # post :create, FactoryBot.create(:user)        
+        # expect(flash[:message]).to eq("Your Account was Created Successfully")
+
+        expect(user_check).to eq(true)
+      end
+  end
 
     describe "GET create" do
-        it "from a create user" do
-          @user = User.create(user_params)
-           user_check = @user.save
-           
-          # post :create, params: user_params   
-          # user = build(:user)
-          # post :create, FactoryBot.create(:user)        
-          # expect(flash[:message]).to eq("Your Account was Created Successfully")
+    it "user with invalid params" do
+    invalid_params = {email: "", password: "123", name: "Ajith"}
+    @user = User.create(invalid_params)
+    user_validate = @user.save
 
-          expect(user_check).to eq(true)
-        end
+    expect(user_validate).to eq(false)
     end
-
+    end
 
   describe "GET Login" do
     it "from a login user" do
-      user_params = {email: 'iop@io.com', password: '123' }
-      @user = User.create(user_params)
-      user_check = User.find_by(email: 'iop@io.com')
+ 
+      user_check = User.find_by(email:"kiran@gmail.com") 
       user = user_check.present?
 
       # post :check_login, params:{email: 'iop@io.com', password: '123' }
       # user = FactoryBot.create(user: user_params)
       # post :check_login, (user)
 
-      expect( user).to eq(true)
+      expect(user).to eq(true)
     end
   end
+
+  describe "GET login" do 
+  it "invalid user login try" do
+  user = User.find_by(email: '')
+  user_validate = user.present?
+
+  expect(user_validate).to eq(false)
+  end
+  end
+
  end
