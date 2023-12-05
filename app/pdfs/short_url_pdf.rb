bea4d120c8990be1e7bb06dcc3865e2f4c7d7c81
Arun <arun.antony@geckolyst.com>
2023-12-05 11:13:58 +0530

    PDF download for respective user

--- app/pdfs/short_url_pdf.rb
+++ app/pdfs/short_url_pdf.rb
@@ -1,15 +1,23 @@
 class ShortUrlPdf < Prawn::Document
+
     def initialize(short_url)
     super()
-    @short_url = ShortUrl.order("id ASC").all
-    table short_url_all
+    @short_url = short_url
+    generate_pdf
     end
+
+    def generate_pdf
+        table short_url_all do
+          row(0).font_style = :bold
+          columns(1..2).align = :center
+        end
+      end
 
 
     def short_url_all
-    [["id","Original url","Shortened url"]]
+    [["user ID","Original url","Shortened url"]] +
     @short_url.map do |short_url|
-        [short_url.original_url,short_url.shortened_url]
+        [short_url.user_id, short_url.original_url, short_url.shortened_url]
     end
     end
 end
