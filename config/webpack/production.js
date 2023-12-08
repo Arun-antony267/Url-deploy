e173de9643adb6397e99aeb2c14a6549e1f44d25
Arun <arun.antony@geckolyst.com>
2023-12-08 14:40:13 +0530

    Update in package.json

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -2,4 +2,11 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const environment = require('./environment')
 
-module.exports = environment.toWebpackConfig()
+module.exports = {
+    entry: './app/javascript/packs/application.js',
+    node: {
+      global: false,
+      __filename: false,
+      __dirname: false,
+    },
+  }
